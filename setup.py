b63286fbd5da1ba8b72787245c84454cf99f69f7
Darian Patzak <my-dev@outlook.de>
2023-10-22 04:28:02 +0200

    installable python package directory setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='database',
+    version='0.1',
+    description='wrapper for sqlite database module',
+    author='Darian Patzak',
+    packages=['database'],
+    zip_safe=False
+)
